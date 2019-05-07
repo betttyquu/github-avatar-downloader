ccce710b76093abf770166de0e578d876a1d1616
betttyquu <absheng947@hotmail.com>
2019-05-07 01:06:02 +0000

    added token access to js

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,28 @@
 var request = require('request');
+var token = require('./secrets.js');
+
+const access_token = process.env.GITHUB_TOKEN;
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    
-}
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'Authorization': token.GITHUB_TOKEN,
+        'User-Agent': 'request'
+      }
+    };
+    request(options, function(err, res, body) {
+    var repos = JSON.parse(body);
+    repos.forEach(function(repo) {
+        console.log(repo.avatar_url);
+      });
+      cb(err, body);
+    });
+  }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-console.log("Errors:", err);
-console.log("Result:", result);
+// console.log("Errors:", err);
+// console.log("Result:", result);
 });
